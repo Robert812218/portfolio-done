bf9ce2535359264ab7dc206c159c4822bb805d21
Robert812218 <robert812@shaw.ca>
2022-12-18 15:21:04 -0700

    set up project

---
+++ src/components/Link.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Link = ({ className, href, children }) => {
+
+    function clickTing(event) {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
+
+        event.preventDefault();
+        window.history.push({}, "", href);
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+    }
+
+    return (
+        <a className={className} href={href} onClick={clickTing}>
+            {children}
+        </a>
+    )
+} 
+
+export default Link;
