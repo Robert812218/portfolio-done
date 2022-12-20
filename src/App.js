0c56fc5582dc830684f96b1de89b3cb3fafcc88e
Robert812218 <robert812@shaw.ca>
2022-12-20 11:13:53 -0700

    set up carousel component

--- src/App.js
+++ src/App.js
@@ -2,37 +2,49 @@ import React, { useState, useEffect } from "react";
 import Languages from './components/Languages';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
-import Projects from './components/Projects';
 import Profile from './components/Profile';
 import NavBar from './components/NavBar';
 import Route from './components/Route';
+import Projects from './components/Projects';
+import Carousel from './components/Carousel/Carousel';
 
-export default function App() {
-  
 
 
+
+const App = () => {
   return (
-    <div>
-      <div>
-        <NavBar />
-      </div>
-      <body>
-        <Route path="/">
-          <Profile />
-        </Route>
-        <Route path="/Languages">
-          <Languages />
-        </Route>
-        <Route path="/Projects">
-          <Projects />
-        </Route>
-        <Route path="/Blog">
-          <Blog />
-        </Route>
-        <Route path="/Contact">
-          <Contact />
-        </Route>
-      </body>
+    <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+      <Carousel>
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+      </Carousel>
     </div>
-  )
+  ) 
 }
+
+export default App;
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <NavBar />
+  //     </div>
+  //     <div>
+  //       <Route path="/">
+  //         <Profile />
+  //       </Route>
+  //       <Route path="/Languages">
+  //         <Languages />
+  //       </Route>
+  //       <Route path="/Projects">
+  //       </Route>
+  //       <Route path="/Blog">
+  //         <Blog />
+  //       </Route>
+  //       <Route path="/Contact">
+  //         <Contact />
+  //       </Route>
+  //     </div>
+  //   </div>
+  // )
