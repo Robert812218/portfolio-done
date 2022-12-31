42a3cc448dc2f0b777a5c79ce3c28143a900dd04
Robert812218 <robert812@shaw.ca>
2022-12-31 11:27:13 -0700

    updated languages

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -1,53 +1,76 @@
-import React, { useState, useFetch } from 'react';
+import React, { useState } from 'react';
 import './Languages.css';
-
-
-
-
-    
 
 export default function Languages() {
     const data = [
 
-        { item: "HTML", icon: "" },
-        { item: "CSS", icon: "" },
-        { item: "Javascript", icon: "" },
-        { item: "React.js", icon: "" },
-        { item: "Typescript", icon: "" },
-        { item: "Elixir", icon: "" },
-        { item: "Phoenix", icon: ""    },
-        { item: "ClojureScript", icon: ""  },
-        { item: "Reagent", icon: ""  },
-        { item: "Flutter", icon: ""  },
-        { item: "Dart", icon: ""  },
-        { item: "Python", icon: ""  },
-        { item: "Django", icon: ""  },
-        { item: "Ruby", icon: ""  },
-        { item: "Rails", icon: ""  },
-        { item: "C#", icon: ""  },
-        { item: ".NET", icon: ""  },
-        { item: "SQL", icon: ""  },
-        { item: "PostgreSQL", icon: ""  },
-        { item: "Vim", icon: ""  },
-        { item: "Git", icon: ""  },
-        { item: "Github", icon: ""  },
-        { item: "Blender", icon: ""  },
-        { item: "SASS", icon: ""  },
-        { item: "Express.js", icon: ""  },
+        { item: "HTML", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+        { item: "CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+        { item: "Javascript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+        { item: "React.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+        { item: "Typescript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+        { item: "Elixir", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/elixir/elixir-original.svg" },
+        { item: "Phoenix", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/phoenix/phoenix-original.svg" },
+        { item: "Rust", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg" },
+        { item: "ClojureScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/clojurescript/clojurescript-original.svg"  },
+        { item: "Clojure", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/clojure/clojure-original.svg"  },
+        { item: "Flutter", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg"  },
+        { item: "Dart", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg"  },
+        { item: "Python", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"  },
+        { item: "Django", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg"  },
+        { item: "Ruby", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg"  },
+        { item: "Rails", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg"  },
+        { item: "C#", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"  },
+        { item: ".NET", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-original.svg"  },
+        { item: "PostgreSQL", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"  },
+        { item: "Vim", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg"  },
+        { item: "Git", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"  },
+        { item: "Github", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"  },
+        { item: "Bootstrap", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },    
+        { item: "Blender", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/blender/blender-original.svg"  },
+        { item: "SASS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"  },
+        { item: "Express.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"  },
+        { item: "Linux", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" },
+        
     ]
 
-    const dataItems = data.map((lang) => 
-        <div key={lang.toString()}>
-            {lang.item}
-        </div>
-    )
+
+
+    
     
 
     return (
-        <div>
-            {data.map((lang) => {
-                return lang.item;
-            })}
+        <div className="languages-container">
+            <h1>Languages, Frameworks and Technologies</h1>
+            <div className="slider-container">
+                <div className="langs-1">
+
+                </div>
+                <div className="langs-2">
+
+                </div>
+                {data.map((lang) => (
+                    <div className="slider-item">
+                        {lang.icon ? 
+                        <div>
+                        <div style={{
+                            backgroundImage: `url("${lang.icon}")`,
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                            backgroundRepeat: "no-repeat",
+                            width: "3rem",
+                            height: "3rem",
+                        }}></div>
+                        {lang.item}</div>
+                        : 
+                        
+                        <div>{lang.item}</div>}
+                    </div>
+                ))}
+            </div>
+            
+            {/* <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" /> */}
+           
         </div>
     );
 }
